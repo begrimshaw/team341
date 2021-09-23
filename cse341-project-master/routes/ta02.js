76285e571ff0dab0c9bbd30b32bb0d904095460b
bgrim <bgrimshaw37@gmail.com>
2021-09-23 11:09:58 -0600

    ta02 push

--- cse341-project-master/routes/ta02.js
+++ cse341-project-master/routes/ta02.js
@@ -2,14 +2,33 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser')
+
+const names = ['peter', 'james', 'john'];
+
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    users: names
   });
 });
+
+router.post('/addUser', (req, res, next) => {
+  names.push(req.body.username);
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const  removeUser = req.body.removeUser;
+  const index = names.indexOf(removeUser);
+  if(index !== -1) {
+    names.splice(index, 1);
+  }
+  res.redirect('/ta02');
+});
+
 
 module.exports = router;
